d987e3767bde23fe7771456ded4c064e69aaf566
Marshall <marshph@wcnet.whitecase.com>
2016-05-31 15:42:34 -0400

    Reorganized this folder

--- White & Case/GUI - Folder Creation Tool.ps1
+++ White & Case/GUI - Folder Creation Tool.ps1
@@ -2,13 +2,30 @@
 #######################################################
 Function Process-Choices
 {
+
+	<#
+	.SYNOPSIS
+		A function to process the data the user put into the form.
+	
+	.DESCRIPTION
+		This function validates data that the user inputted into the form to
+        verify it is acceptable for the folder creation function.
+	
+	.EXAMPLE
+		Process-Choices
+	
+	.NOTES
+		N/A
+#>
+
     $Invalid = $False
     $Loggingbox.clear()
 
     ################
     #Validate Share#
     ################
-
+    
+    #If the share dropdown is null this will output the problem to the Rich Text Box.
     Try
     {
         $ChosenShare = $ShareDropDownBox.SelectedItem.ToString()
@@ -24,6 +41,7 @@ Function Process-Choices
     #Validate Region#
     #################
 
+    #If the region dropdown is null this will output the problem to the Rich Text Box.
     Try
     {
         $ChosenRegion = $RegionDropDownBox.SelectedItem.ToString()
@@ -39,6 +57,14 @@ Function Process-Choices
     #Validate Client Number#
     ########################
 
+    <#
+    If the Client Number Text Box is null or empty this will output the problem 
+    to the Rich Text Box. Client Numbers are allowed to have hyphens so if one is
+    present we temporarily strip it out to validate the rest of the string is a 
+    valid INT. If either of these processes fail we output the problem to the Rich
+    Text Box.
+    #>
+
     $ChosenClientNumber = $ClientNumberTextBox.text
     IF([string]::IsNullOrWhiteSpace($ChosenClientNumber))
     {    
@@ -46,7 +72,8 @@ Function Process-Choices
         $Invalid =$True  
     }
 
-    If(($ChosenClientNumber -as [Int]) -isnot [Int])
+    $Intcheck = $ChosenClientNumber -replace "-",""
+    If(($Intcheck -as [Int]) -isnot [Int])
     {
         Write-RichText -LogType Error -LogMsg "`t The Client Number given is not all numeric."
         $Invalid =$True         
@@ -55,6 +82,14 @@ Function Process-Choices
     ######################
     #Validate Client Name#
     ######################
+
+    <#
+    If the Client Name Text Box is null or empty this will output the problem
+    to the Rich Text Box. We also trim the entry to 20 characters as requested 
+    and remove any special characters and replace them with underscores. 
+    However this is only temporary as we should be pulling this from DMS directly 
+    in the near future.
+    #>
 
     $ChosenClientName = $ClientNameTextBox.text
     IF([string]::IsNullOrWhiteSpace($ChosenClientName))
@@ -70,41 +105,14 @@ Function Process-Choices
             $ChosenClientName = $ChosenClientName.substring(0,20)
         }
         
-        #################################################################
-        #Handle all possible special characters that should be replaced.#
-        #################################################################
 
-        $ChosenClientName = $ChosenClientName.replace("!","_")
-        $ChosenClientName = $ChosenClientName.replace("@","_")
-        $ChosenClientName = $ChosenClientName.replace("#","_")
-        $ChosenClientName = $ChosenClientName.replace("$","_")
-        $ChosenClientName = $ChosenClientName.replace("%","_")
-        $ChosenClientName = $ChosenClientName.replace("^","_")
-        $ChosenClientName = $ChosenClientName.replace("&","_")
-        $ChosenClientName = $ChosenClientName.replace("*","_")
-        $ChosenClientName = $ChosenClientName.replace("(","_")
-        $ChosenClientName = $ChosenClientName.replace(")","_")
-        $ChosenClientName = $ChosenClientName.replace("{","_")
-        $ChosenClientName = $ChosenClientName.replace("}","_")
-        $ChosenClientName = $ChosenClientName.replace("[","_")
-        $ChosenClientName = $ChosenClientName.replace("]","_")
-        $ChosenClientName = $ChosenClientName.replace("`"","_")
-        $ChosenClientName = $ChosenClientName.replace("`'","_")
-        $ChosenClientName = $ChosenClientName.replace(":","_")
-        $ChosenClientName = $ChosenClientName.replace(";","_")
-        $ChosenClientName = $ChosenClientName.replace(",","_")
-        $ChosenClientName = $ChosenClientName.replace(".","_")
-        $ChosenClientName = $ChosenClientName.replace("<","_")
-        $ChosenClientName = $ChosenClientName.replace(">","_")
-        $ChosenClientName = $ChosenClientName.replace("/","_")
-        $ChosenClientName = $ChosenClientName.replace("|","_")
-        $ChosenClientName = $ChosenClientName.replace("\","_")
-        $ChosenClientName = $ChosenClientName.replace("-","_")
-        $ChosenClientName = $ChosenClientName.replace("+","_")
-        $ChosenClientName = $ChosenClientName.replace("=","_")
-        $ChosenClientName = $ChosenClientName.replace("``","_")
-        $ChosenClientName = $ChosenClientName.replace("~","_")
-        $ChosenClientName = $ChosenClientName.replace(" ","_")
+        <#
+        Handle all possible special characters that should be replaced.
+        This regex replaces any character that is not a letter or a number with an underscore.
+        #>
+
+        $pattern = '[^a-zA-Z\d]'
+        $ChosenClientName =  $ChosenClientName -replace $Pattern, '_' 
 
     }
 
@@ -125,8 +133,50 @@ Function Process-Choices
     }
 }
 
-function Write-RichText
+Function Clear-Choices
 {
+	<#
+	.SYNOPSIS
+		A function to clear all user input
+	
+	.DESCRIPTION
+		This function is designed to clear all user-generated input from the form.
+	
+	.EXAMPLE
+		Clear-Choices
+	
+	.NOTES
+		N/A
+#>
+	
+    $ShareDropDownBox.SelectedIndex = -1
+    $ShareDropDownBox.SelectedIndex = -1
+    $RegionDropDownBox.SelectedIndex = -1
+    $RegionDropDownBox.SelectedIndex = -1
+    $ClientNumberTextBox.Text = ''
+    $ClientNameTextBox.Text = ''
+    $LoggingBox.Text = ''
+}
+
+Function Write-RichText
+{
+
+	<#
+	.SYNOPSIS
+		A function to output text to a Rich Text Box.
+	
+	.DESCRIPTION
+		This function appends text to a Rich Text Box and colors it based 
+        upon the type of message being displayed.
+	
+	.EXAMPLE
+		Write-Richtext -LogType Error -LogMsg "This is an Error."
+		Write-Richtext -LogType Success -LogMsg "This is a Success."
+		Write-Richtext -LogType Informational -LogMsg "This is Informational."
+	
+	.NOTES
+		N/A
+#>
 
     Param
     (
@@ -158,22 +208,50 @@ function Write-RichText
          
 }
 
-function Create-Share
+Function Create-Share
 {
+
+	<#
+	.SYNOPSIS
+		A function to create the requested DFS share.
+	
+	.DESCRIPTION
+		This function takes all of the validated user input from the form and
+        creates the required share if it does not already exist.
+    
+    .PARAMETER Share
+        This is the user selected share from the dropdown box.
+
+    .PARAMETER Region
+        This is the user selected region from the dropdown box.
+
+    .PARAMETER FolderName
+        This is the combination of the client number and name.
+
+    .PARAMETER Credential
+        The Credential Object used to create the share.
+	
+	.EXAMPLE
+		Create-Share -Share Admin -Region Americas -Foldername 10101_That_Client -Credential $Credential
+	
+	.NOTES
+		N/A
+#>
+
     Param
     (
     	[Parameter(Mandatory = $true,Position = 0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 		[String]$Share, 
     	[Parameter(Mandatory = $true,Position = 1,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 		[String]$Region,
-    	[Parameter(Mandatory = $true,Position = 1,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    	[Parameter(Mandatory = $true,Position = 2,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 		[String]$FolderName,
-    	[Parameter(Mandatory = $true,Position = 1,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    	[Parameter(Mandatory = $true,Position = 3,ValueFromPipeline,ValueFromPipelineByPropertyName)]
 		[Object]$Credential
     )
     
     $CompletePath = "\\WCNET\Firm\$Share\$Region\$FolderName"
-    #$CompletePath = "C:\windows\temp\$Share\$Region\$FolderName"
+    
     
     If(Test-Path $CompletePath)
     {
@@ -266,6 +344,7 @@ Function Write-Log
 $Form = New-Object System.Windows.Forms.Form    
 $Form.Size = New-Object System.Drawing.Size(800,500)
 $Form.text = "White & Case Share Creation Tool"
+$Form.backcolor = "gray"
 
 #endregion
 
@@ -303,7 +382,7 @@ $Form.Controls.Add($ShareLabel)
 #Create the Dropdown - Choose Region#
 #####################################
 $RegionDropDownBox = New-Object System.Windows.Forms.ComboBox 
-$RegionDropDownBox.Location = New-Object System.Drawing.Size(20,100)       
+$RegionDropDownBox.Location = New-Object System.Drawing.Size(175,50)       
 $RegionDropDownBox.Size = New-Object System.Drawing.Size(120,25)            
 $RegionDropDownBox.DropDownHeight = 200
 $RegionDropDownBox.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList                                     
@@ -318,7 +397,7 @@ Foreach ($Region in $RegionList)
 #Create the Label for Choose Region#
 ####################################
 $ShareLabel = New-Object System.Windows.Forms.Label
-$ShareLabel.Location = New-Object System.Drawing.Point(20, 80)
+$ShareLabel.Location = New-Object System.Drawing.Point(175,30)
 $ShareLabel.Size = New-Object System.Drawing.Size(150, 25)
 $ShareLabel.Text = "Choose the Region"
 $Form.Controls.Add($ShareLabel)
@@ -331,8 +410,8 @@ $Form.Controls.Add($ShareLabel)
 #Create the Submit Button#
 ##########################
 $SubmitButton = New-Object System.Windows.Forms.Button 
-$SubmitButton.Location = New-Object System.Drawing.Size(600,30) 
-$SubmitButton.Size = New-Object System.Drawing.Size(130,100) 
+$SubmitButton.Location = New-Object System.Drawing.Size(640,30) 
+$SubmitButton.Size = New-Object System.Drawing.Size(130,50) 
 $SubmitButton.Text = "Submit"
 $SubmitButton.Enabled = $True
 $SubmitButton.Add_Click({Process-Choices})
@@ -341,20 +420,38 @@ $Form.Controls.Add($SubmitButton)
 
 #endregion
 
+#region ClearButton
+
+#########################
+#Create the Clear Button#
+#########################
+$ClearButton = New-Object System.Windows.Forms.Button 
+$ClearButton.Location = New-Object System.Drawing.Size(640,90) 
+$ClearButton.Size = New-Object System.Drawing.Size(130,50) 
+$ClearButton.Text = "Clear"
+$ClearButton.Enabled = $True
+$ClearButton.Add_Click({Clear-Choices})
+
+$Form.Controls.Add($ClearButton)
+
+#endregion
+
 #region ClientNumberInputBox
+
 ####################################
 #Create the Client Number Input Box#
 ####################################
 $ClientNumberTextBox = New-Object System.Windows.Forms.TextBox 
-$ClientNumberTextBox.Location = New-Object System.Drawing.Size(20,150) 
+$ClientNumberTextBox.Location = New-Object System.Drawing.Size(325,50) 
 $ClientNumberTextBox.Size = New-Object System.Drawing.Size(260,20) 
+$ClientNumberTextBox.BackColor = "White"
 $Form.Controls.Add($ClientNumberTextBox) 
 
 ##################################################
 #Create the Label for the Client Number Input Box#
 ##################################################
 $ShareLabel = New-Object System.Windows.Forms.Label
-$ShareLabel.Location = New-Object System.Drawing.Point(20,130)
+$ShareLabel.Location = New-Object System.Drawing.Point(325,30)
 $ShareLabel.Size = New-Object System.Drawing.Size(260, 25)
 $ShareLabel.Text = "Type in the Client Number"
 $Form.Controls.Add($ShareLabel)
@@ -367,30 +464,36 @@ $Form.Controls.Add($ShareLabel)
 #Create the Client Name Input Box#
 ##################################
 $ClientNameTextBox = New-Object System.Windows.Forms.TextBox 
-$ClientNameTextBox.Location = New-Object System.Drawing.Size(20,200) 
-$ClientNameTextBox.Size = New-Object System.Drawing.Size(260,20) 
+$ClientNameTextBox.Location = New-Object System.Drawing.Size(325,110) 
+$ClientNameTextBox.Size = New-Object System.Drawing.Size(260,20)
+$ClientNameTextBox.BackColor = "White"
 $Form.Controls.Add($ClientNameTextBox) 
 
 ################################################
 #Create the Label for the Client Name Input Box#
 ################################################
 $ShareLabel = New-Object System.Windows.Forms.Label
-$ShareLabel.Location = New-Object System.Drawing.Point(20,180)
+$ShareLabel.Location = New-Object System.Drawing.Point(325,90)
 $ShareLabel.Size = New-Object System.Drawing.Size(260, 25)
 $ShareLabel.Text = "Type in the Client Name"
+$ShareLabel.ForeColor = 'Black'
 $Form.Controls.Add($ShareLabel)
 
 #endregion
 
 #region LoggingBox
 
+########################
+#Create the Logging Box#
+########################
 $Loggingbox = New-Object 'System.Windows.Forms.RichTextBox'
-$Loggingbox.Location = New-Object System.Drawing.Size(300,150) 
-$Loggingbox.Size = New-Object System.Drawing.Point(475,300) 
+$Loggingbox.Location = New-Object System.Drawing.Size(20,150) 
+$Loggingbox.Size = New-Object System.Drawing.Point(750,300) 
 $Loggingbox.Name = "Output Box"
 $Loggingbox.TabIndex = 2
 $Loggingbox.Text = ""
 $Loggingbox.font = "Arial"
+$Loggingbox.BackColor = "White"
 $Form.Controls.Add($Loggingbox)
 
 #endregion
---
+++ White & Case/Jenkins - Disable SQL Backup Jobs.ps1
@@ -0,0 +1,50 @@
+﻿$secpasswd = ConvertTo-SecureString $ENV:ADMPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($ENV:ADMUsername, $secpasswd)
+$Target = $ENV:SQLInstance
+
+
+Invoke-Command -ScriptBlock {
+
+$cSQLInstance = "AM1FMDB903\FINDATA"
+
+<# 
+Flag to determine job status
+OPTION:
+0 - Disabled
+1 - Enabled
+#>
+$bJobStatus = 1
+
+#SQL Statement
+$cSQLStmt = @"
+DECLARE
+	@bExecSQL BIT
+	, @bJobSwitch BIT
+	, @cSQLStmt NVARCHAR(2048);
+
+SET @bExecSQL = 1;
+SET @bJobSwitch = $bJobStatus;
+SET @cSQLStmt = '';
+
+SELECT
+	@cSQLStmt = (
+		@cSQLStmt 
+		+ 'EXEC [msdb].[dbo].[sp_update_job] @job_id = N''' + CAST([s].[job_id] AS NVARCHAR(36)) + ''', @enabled = ' + CAST(@bJobSwitch AS NCHAR(1)) + ';' 		
+		+ ' /*' 
+		+ [s].[name]
+		+ '*/' 
+		+ NCHAR(13)
+	)
+FROM [msdb].[dbo].[sysjobs] AS [s]
+WHERE [s].[name] LIKE '%FULL%'
+AND [s].[name] NOT LIKE '%SYSTEM_DATABASES%';
+
+IF @bExecSQL = 1
+	EXEC [sys].[sp_executesql] @cSQLStmt;
+ELSE
+	PRINT @cSQLStmt;
+"@
+
+Invoke-Sqlcmd -Query $cSQLStmt -ServerInstance $($Target + "\FINDATA") -AbortOnError -Verbose
+
+} -ComputerName $Target -Credential $Credential
