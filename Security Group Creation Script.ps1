fe0f3f14795dd9135ab64d954f0d081e877fee01
Marshall <marshph@wcnet.whitecase.com>
2016-08-18 10:22:11 -0400

    Changes galore

--- Security Group Creation Script.ps1
+++ Security Group Creation Script.ps1
@@ -1,16 +1,66 @@
 ﻿#region Variable Declarations
-$SecPassword = ConvertTo-SecureString "$ENV:SAPassword" -AsPlainText -Force
-$GLOCred = New-Object System.Management.Automation.PSCredential ($ENV:SAUsername, $SecPassword)
+#$SecPassword = ConvertTo-SecureString "$ENV:SAPassword" -AsPlainText -Force
+#$GLOCred = New-Object System.Management.Automation.PSCredential ($ENV:SAUsername, $SecPassword)
+$GLOCred = Get-Credential
 $Date = Get-Date -Uformat %Y-%m-%d
-$Reportpath = "D:\Job Output\Automated Security Groups\SGCReport - $Date.xlsx"
+#$Reportpath = "D:\Job Output\Automated Security Groups\SGCReport - $Date.xlsx"
+$Reportpath = "C:\Temp\SGCReport - $Date.xlsx"
 #endregion
 
 #region Import Modules
+#AD Module Used for Queries. PSExcel module used for reporting and outputting all the data to excel.
+
 Import-Module ActiveDirectory
 Import-Module PSExcel
+
+#endregion
+
+#region Function Declarations
+
+function Get-SiteCode{
+
+    Param(
+    
+        [String]$Office
+    )
+        $Office = Fix-Jabroni -Office $Office
+        $Description = Get-ADOrganizationalUnit -Filter {Name -eq $Office } -Properties Description  | select -ExpandProperty Description
+        $SiteCode = ([regex]::matches($Description, "(?:Site Code:)(\w{3})")).groups[1].value
+
+        If($Sitecode){
+            Return $SiteCode
+        }
+
+        Else{
+            Return "Failed Query or Parse"
+        }
+}
+
+function Fix-Jabroni{
+    
+    #Thanks Kervin
+    Param(
+    
+        [String]$Office
+    )
+
+    switch ($Office)
+    {
+        'Düsseldorf'      {$Office = 'Duesseldorf'}
+        'Manila (DSM)'    {$Office = 'Manila'}
+        'Silicon Valley'  {$office = 'Palo Alto'}
+        'Washington D.C.' {$Office = 'Washington DC'}
+        Default {}
+    }
+
+    Return $Office
+
+}
+
 #endregion
 
 #region Gather SQl Data
+
 $Session = New-PsSession -ComputerName 'am1mfdb001' -Credential $GLOCred
 
 If (!$Session){
@@ -58,47 +108,72 @@ $Query = Invoke-Command -ComputerName 'am1mfdb001' -Credential $GLOCred -ScriptB
 	}
 
 Get-PSSession | Remove-PSSession
+
 #endregion
 
 #region Verify SQL Data
+
 If(!$Query[0].OfficeName -or !$Query[0].emailaddress)
 {
     Write-output "Returned Data appears to be in the wrong format."
     exit;
 }
+
 #endregion
 
 #region Gather Unique offices and Security Groups
 
-$UniqueOffices = $Query.officename | select -Unique | Sort-Object
-Write-output "$($Uniqueoffices.count) unique office names detected."
-$SecurityGroups = Get-ADGroup -Filter {Name -like "*(ASG)*"}
-Write-output "$($SecurityGroups.count) Security Groups detected."
+$UniqueOffices = $Query.officename | Where-object {$_ -ne 'remote'} | select -Unique | Sort-Object
+$SecurityGroups = Get-ADGroup -Filter {Name -like "*-P-AllOffice"}
+
 #endregion
 
 #region Create Missing Security Groups
 
 $newgroups = @()
+$LocCodes = @()
 
 Foreach ($Office in $UniqueOffices)
 {
-    If((($SecurityGroups | Where-Object {$_.samaccountname -eq $Office + '(ASG)'} | measure-object).count) -gt 0)
-    {}
+    [String]$SiteCode = Get-SiteCode -Office $Office
 
-    Else
-    {
-        $NewGroups += $Office
-        
-        New-ADGroup -Name "$Office(ASG)" `
-			-SamAccountName "$Office(ASG)" `
-			-GroupCategory Security `
-			-GroupScope Global `
-			-DisplayName "$Office(ASG)" `
-			-Path "OU=ODSBased Groups,OU=Security Groups,OU=FIRMWIDE,DC=WCNET,DC=whitecase,DC=com" `
-			-Description "Members of this group are in the $Office Location" `
-			-Credential $GLOCred
+    If($SiteCode -eq "Failed Query or Parse"){
+    
+        Write-Output "$Office was not able to be parsed."
     }
+    
+    Else {
+
+        $Object = New-Object PSObject -Property @{
+
+        Office       = $Office                
+        SiteCode     = $SiteCode 
+        }
+
+        $LocCodes += $Object
+
+        If((($SecurityGroups | Where-Object {$_.samaccountname -eq $SiteCode + '-p-AllOffice'} | measure-object).count) -gt 0)
+        {}
+
+        Else{
+        
+            $NewGroups += $SiteCode + '-P-AllOffice'
+        
+            New-ADGroup -Name $($SiteCode + '-P-AllOffice') `
+			    -SamAccountName $($SiteCode + '-P-AllOffice') `
+			    -GroupCategory Security `
+			    -GroupScope Global `
+			    -DisplayName $($SiteCode + '-P-AllOffice') `
+			    -Path "OU=Automated Groups,OU=Security Groups,OU=FIRMWIDE,DC=WCNET,DC=whitecase,DC=com" `
+			    -Description "Automated Group created by leveraging ODS." `
+			    -Credential $GLOCred
+        }
+
+    }
+
 }
+
+
 #endregion
 
 #region Verify New Creations
@@ -109,16 +184,16 @@ If($NewGroups)
 
     Foreach($Group in $NewGroups)
     {
-        $Created = Get-ADgroup -Identity "$Group(ASG)"
+        $Created = Get-ADgroup -Identity $Group
 
         If($Created)
         {
-            Write-output "$($Group + '(ASG)') Created Successfully"
+            Write-output "$($Group) Created Successfully"
         }
 
         Else
         {
-            Write-output "Failed to create $($Group + '(ASG)')"
+            Write-output "Failed to create $($Group)"
         }
     }
 }
@@ -126,10 +201,12 @@ If($NewGroups)
 #endregion
 
 #region Determine Valid AD Users
+
 $ADUsers = @()
 $nonadusers = @()
+$FilteredUsers = $Query | Where-Object {$_.officename -ne 'remote'}
 
-Foreach($User in $Query)
+Foreach($User in $FilteredUsers)
 {
     Try
     {
@@ -151,7 +228,8 @@ Foreach($User in $Query)
 
         Else
         {
-            Add-Member -InputObject $UserCheck -MemberType NoteProperty -Name ODSOffice -Value $User.OfficeName -Force
+            $SiteCode = $LocCodes | Where-Object {$_.office -eq $User.OfficeName} | select -ExpandProperty SiteCode
+            Add-Member -InputObject $UserCheck -MemberType NoteProperty -Name SiteCode -Value $Sitecode -Force
             $adusers += $UserCheck
         }
     }
@@ -162,11 +240,11 @@ Foreach($User in $Query)
 
 #region Add missing members to each group
 
-$ADGroups = Get-ADGroup -Filter {Name -like "*(ASG)*"}
+$ADGroups = Get-ADGroup -Filter {Name -like "*-P-AllOffice*"}
 
 Foreach($Group in $ADGroups)
 {
-    $LocationUsers = $ADUsers | Where-Object {$Group.name -like "*$($_.ODSOffice)*"}
+    $LocationUsers = $ADUsers | Where-Object {$Group.name -like "*$($_.SiteCode)*"}
 
     Try
     {
@@ -182,10 +260,8 @@ Foreach($Group in $ADGroups)
 #endregion
 
 #region Reporting Stuff
-
-$Madegroups = get-adgroup -Filter {name -like "*(ASG)*"}
-
-Foreach($Group in $MadeGroups)
+$ADGroups = $ADGroups | Sort-Object
+Foreach($Group in $ADGroups)
 {
     $Members = Get-ADGroupMember -Identity $Group | select -Property Name,SamAccountName
     $members | Export-XLSX -Path $Reportpath -worksheetname $Group.Name
@@ -194,4 +270,4 @@ Foreach($Group in $MadeGroups)
 #endregion
 
 #To Delete the test groups
-#get-adgroup -Filter {name -like "*(ASG)*"} | Remove-ADGroup -Confirm:$False -Credential $GLOCred
+#get-adgroup -Filter {name -like "*-P-AllOffice*"} | Remove-ADGroup -Confirm:$False -Credential $GLOCred
